12eef281f89cb8f110ef491da61c0858f81c3e56
Alan <alannguyen80@gmail.com>
2018-12-27 20:24:33 -0500

    Add map to cache addresses

--- JdbcSample/src/main/java/edu/drexel/dao/AddressDAO.java
+++ JdbcSample/src/main/java/edu/drexel/dao/AddressDAO.java
@@ -1,10 +1,14 @@
 package edu.drexel.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import edu.drexel.model.Address;
 
 public interface AddressDAO {
+	static Map<Integer, Address> ADDRESS_MAP = new HashMap<>();
+	
 	List<Address> getAll();
 	Address findByID(int id);
 	int insert(Address address);
